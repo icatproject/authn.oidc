fd907f28606d5bc33e42310cbeb02f7e1254446d
Emil Junker <emil.junker@outlook.com>
2021-01-25 17:44:41 +0100

    use TimerTask to check openidConfiguration

--- src/main/java/org/icatproject/authn_oidc/OpenidConfigurationManager.java
+++ src/main/java/org/icatproject/authn_oidc/OpenidConfigurationManager.java
@@ -7,6 +7,8 @@ import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.json.Json;
 import javax.json.JsonException;
@@ -16,15 +18,42 @@ import javax.json.JsonReader;
 import com.auth0.jwk.JwkProvider;
 import com.auth0.jwk.JwkProviderBuilder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class OpenidConfigurationManager {
+
+    public class Action extends TimerTask {
+
+        @Override
+        public void run() {
+            Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+            try {
+                checkJwkProvider();
+            } catch (RuntimeException e) {
+                logger.error(e.getMessage());
+                timer.schedule(new Action(), shortIntervalMillis);
+            } finally {
+                timer.schedule(new Action(), intervalMillis);
+            }
+        }
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(OpenidConfigurationManager.class);
 
     private URL openidConfigurationUrl;
     private String tokenIssuer;
     private JwkProvider jwkProvider;
 
+    private long intervalMillis = 86400000L; // every 24 hours
+    private long shortIntervalMillis = 60000L; // every minute
+    private Timer timer = new Timer();
+
     public OpenidConfigurationManager(String wellKnownUrl, String issuer) throws MalformedURLException {
         openidConfigurationUrl = new URL(wellKnownUrl);
         tokenIssuer = issuer;
+
+        timer.schedule(new Action(), shortIntervalMillis);
     }
 
     public String getTokenIssuer() {
@@ -50,7 +79,7 @@ public class OpenidConfigurationManager {
             jsonResponse = jsonReader.readObject();
         } catch (IOException | JsonException e) {
             String msg = "Unable to obtain information from the wellKnownUrl in run.properties: " + e.getMessage();
-            throw new IllegalArgumentException(msg);
+            throw new RuntimeException(msg);
         }
 
         String issuer;
@@ -60,12 +89,12 @@ public class OpenidConfigurationManager {
             issuer = jsonResponse.getString("issuer");
         } catch (NullPointerException | MalformedURLException e) {
             String msg = "Unable to obtain jwk provider or issuer: " + e.getMessage();
-            throw new IllegalArgumentException(msg);
+            throw new RuntimeException(msg);
         }
 
         if (!tokenIssuer.equals(issuer)) {
             String msg = "The issuer in the well-known configuration does not match the tokenIssuer in run.properties.";
-            throw new IllegalArgumentException(msg);
+            throw new RuntimeException(msg);
         }
     }
 
