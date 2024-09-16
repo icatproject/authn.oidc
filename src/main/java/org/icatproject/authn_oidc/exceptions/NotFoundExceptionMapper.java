ff4e3166df56635c74852b488a61d2d556f2f01c
Alex Kemp <alexander.kemp@stfc.ac.uk>
2024-09-16 14:38:31 +0100

    Tidy up

--- src/main/java/org/icatproject/authn_oidc/exceptions/NotFoundExceptionMapper.java
+++ src/main/java/org/icatproject/authn_oidc/exceptions/NotFoundExceptionMapper.java
@@ -1,21 +1,20 @@
 package org.icatproject.authn_oidc.exceptions;
 
-import java.io.ByteArrayOutputStream;
-
 import jakarta.json.Json;
 import jakarta.json.stream.JsonGenerator;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
 
 @Provider
 public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
 
-	private static Logger logger = LoggerFactory.getLogger(NotFoundExceptionMapper.class);
+	private static final Logger logger = LoggerFactory.getLogger(NotFoundExceptionMapper.class);
 
 	@Override
 	public Response toResponse(NotFoundException e) {
